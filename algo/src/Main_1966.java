90cf85295f236bf6cc3edd8e38cc14b8750adb3e
polarbearHam <gkagu12@naver.com>
2023-04-17 09:37:53 +0900

    4/17

--- algo/src/Main_1966.java
+++ algo/src/Main_1966.java
@@ -2,12 +2,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_1966 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
 		int T = Integer.parseInt(br.readLine());
 		
@@ -16,23 +19,38 @@ public class Main_1966 {
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
 			
+			LinkedList<int[]> q= new LinkedList<>();
 			st = new StringTokenizer(br.readLine(), " ");
-			Queue<Integer> q = new ArrayDeque<>();
-			int max = 0;
-			int[] important = new int[N];
-			for(int i=0; i<N; i++) {
-				int a = Integer.parseInt(st.nextToken());
-				important[i] = a;
-				q.add(a);
+			
+			for (int i = 0; i < N; i++) {
+				q.offer(new int[] {i, Integer.parseInt(st.nextToken())});
 			}
-			Arrays.sort(important);
-			int impIdx = 0;
-			int idx = M;
-			while(true) {
-				if(q.peek()==important[impIdx]) {
-					q.poll();
+			int count=0;
+			while(q.size()>0) {
+				int[] front = q.poll();
+				boolean isMax = true;
+				
+				for (int i = 0; i < q.size(); i++) {
+					if(front[1]<q.get(i)[1]) {
+						q.offer(front);
+						for (int j = 0; j < i; j++) {
+							q.offer(q.poll());
+						}
+						isMax = false;
+						break;
+					}
+				}
+				if(isMax==false) {
+					continue;
+				}
+				count++;
+				if(front[0]==M) {
+					break;
 				}
 			}
+			sb.append(count).append("\n");
+			
 		}
+		System.out.println(sb);
 	}
 }
