ad074a78f20bb160ee19c7d58b953f1810e8ed18
HAM <gkagu12@naver.com>
2023-07-16 23:54:56 +0900

    07/16

---
+++ day1212/src/Main_1110.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_1110 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int cnt = 0;
+		int copy = N;
+		
+		do {
+			N = ((N%10)*10) + (((N/10)+(N%10))%10);
+			cnt++;
+		}while(copy !=N);
+		
+		System.out.println(cnt);
+	}
+}
