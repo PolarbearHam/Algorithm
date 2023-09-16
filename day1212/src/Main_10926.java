76c7d931c808fdaf8a7185ed891fb6f5359466c0
HAM <gkagu12@naver.com>
2023-09-16 16:00:22 +0900

    09/16

---
+++ day1212/src/Main_10926.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_10926 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(br.readLine()+"??!");
+	}
+}
