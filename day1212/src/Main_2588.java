254f2e1976332f86553879cbacec4682b48bb1b2
HAM <gkagu12@naver.com>
2023-09-19 03:59:07 +0900

    09/19

---
+++ day1212/src/Main_2588.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_2588 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(br.readLine());
+		String s = br.readLine();
+		int B = Integer.parseInt(s);
+		int b1 = s.charAt(0)-48;
+		int b2 = s.charAt(1)-48;
+		int b3 = s.charAt(2)-48;
+		
+		System.out.println(A*b3);
+		System.out.println(A*b2);
+		System.out.println(A*b1);
+		System.out.println(A*B);
+		
+	}
+}
