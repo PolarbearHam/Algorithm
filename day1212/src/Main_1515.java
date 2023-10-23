da3d1951fe194c194f2be329e0fc6caa9f16d7d9
HAM <gkagu12@naver.com>
2023-10-23 14:10:14 +0900

    10/23

--- day1212/src/Main_1515.java
+++ day1212/src/Main_1515.java
@@ -1,9 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main_1515 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int pt = 0;
+		
+		int base = 0;
+		while(base++<=30000) {
+			String tmp = String.valueOf(base);
+			for(int i=0; i<tmp.length(); i++) {
+				if(tmp.charAt(i)==s.charAt(pt)) {
+					pt++;
+				}
+				if(pt==s.length()) {
+					System.out.println(base);
+					return;
+				}
+			}
+		}
+		
 	}
 
 }
