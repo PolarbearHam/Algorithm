d8f6db1c5e72a4a860c751514575b7aca6eda01e
HAM <gkagu12@naver.com>
2023-11-08 14:59:36 +0900

    11/08

---
+++ day1212/src/Main_1058.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_1058 {
+	static int N;
+	static int arr[][];
+	static int max = Integer.MAX_VALUE>>2;
+	static int ans;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N+1][N+1];
+		
+		for(int i=1; i<=N; i++) {
+			String str = br.readLine();
+			
+			for(int j=1; j<=N; j++) {
+				char ch = str.charAt(j-1);
+				
+				if(ch=='Y') arr[i][j]=1;
+				else if(i!=j) arr[i][j] = max;
+			}
+		}
+		
+		for(int k=1; k<=N; k++) {
+			for(int i=1; i<=N; i++) {
+				for(int j=1; j<=N; j++) {
+					if(i==j || j==k || i==k) continue;
+					else if(arr[i][j] > arr[i][k]+arr[k][j]) arr[i][j] = arr[i][k]+arr[k][j];
+				}
+			}
+		}
+		
+		for(int i=1; i<=N; i++) {
+			int tmp = 0;
+			
+			for(int j=1; j<=N; j++) {
+				if(i==j) continue;
+				else if(arr[i][j] <= 2) tmp++;
+			}
+			ans = Math.max(ans, tmp);
+		}
+		System.out.println(ans);
+	}
+}
