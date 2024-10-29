07eb15ce968e013cbea9c40a1d7b17a67394fe82
HAM <gkagu12@naver.com>
2024-10-30 05:26:57 +0900

    10/29

---
+++ day1212/src/Main_11404.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11404 {
+	static final int INF = 987654321;
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[n+1][n+1];
+		for(int i=0; i<n+1; i++) {
+			for(int j=0; j<n+1; j++) {
+				arr[i][j] = INF;
+				if(i==j) arr[i][j]=0;
+			}
+		}
+		StringTokenizer st;
+		
+		for(int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			arr[s][e] = Math.min(arr[s][e], c);
+		}
+		
+		for(int k=1; k<n+1; k++) {
+			for(int i=1; i<n+1; i++) {
+				for(int j=1; j<n+1; j++) {
+					if(arr[i][j]>arr[i][k]+arr[k][j]) {
+						arr[i][j] = arr[i][k]+arr[k][j];
+					}
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=1; i<n+1; i++) {
+			for(int j=1; j<n+1; j++) {
+				sb.append(arr[i][j]+" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
--- day1212/src/Main_1753.java
+++ day1212/src/Main_1753.java
@@ -1,16 +1,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main_1753 {
 	static int V, E;
-	static int INF = Integer.MAX_VALUE;
 	static int[] dist;
-	static List<Node>[] list;
+	static ArrayList<Node>[] list;
+	static int INF = 987654321;
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,14 +16,12 @@ public class Main_1753 {
 		
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
+		
+		int start = Integer.parseInt(br.readLine());
 		dist = new int[V+1];
 		list = new ArrayList[V+1];
-		int start = Integer.parseInt(br.readLine());
-		
-		Arrays.fill(dist, INF);
-		
-		
-		for(int i=1; i<V+1; i++) {
+		for(int i=0; i<V+1; i++) {
+			dist[i] = INF;
 			list[i] = new ArrayList<Node>();
 		}
 		
@@ -36,53 +32,48 @@ public class Main_1753 {
 			int c = Integer.parseInt(st.nextToken());
 			list[s].add(new Node(e, c));
 		}
-		
-		StringBuilder sb = new StringBuilder();
-		
 		dijkstra(start);
+		StringBuilder sb = new StringBuilder();
 		
 		for(int i=1; i<V+1; i++) {
 			if(dist[i]==INF) sb.append("INF\n");
 			else sb.append(dist[i]+"\n");
 		}
-		System.out.println(sb);
-		br.close();
-
-	}
-	
-	public static class Node implements Comparable<Node>{
-		int end;
-		int cost;
-		public Node(int end, int cost) {
-			this.end = end;
-			this.cost = cost;
-		}
-		
-		@Override
-		public int compareTo(Node o) {
-			return cost - o.cost;
-		}
+		System.out.println(sb.toString());
 	}
 	
 	public static void dijkstra(int start) {
-		PriorityQueue<Node> queue = new PriorityQueue<>();
-		boolean[] visited = new boolean[V+1];
-		queue.add(new Node(start, 0));
+		PriorityQueue<Node> q= new PriorityQueue<>();
 		dist[start] = 0;
+		boolean[] visited = new boolean[V+1];
+		q.add(new Node(start, 0));
 		
-		while(queue.size()>0) {
-			Node cur = queue.poll();
-			int now = cur.end;
-			
-			if(visited[now]==true) continue;
+		while(q.size()>0) {
+			Node n = q.poll();
+			int now = n.e;
+			if(visited[now]) continue;
 			visited[now] = true;
 			
-			for(Node n : list[now]) {
-				if(dist[n.end]>dist[now]+n.cost) {
-					dist[n.end] = dist[now]+n.cost;
-					queue.add(new Node(n.end, dist[n.end]));
+			for(Node node : list[now]) {
+				if(dist[node.e]>dist[now]+n.c) {
+					dist[node.e]= dist[now]+n.c;
+					q.add(new Node(node.e, dist[node.e]));
 				}
 			}
+		}
+		
+	}
+	
+	public static class Node implements Comparable<Node>{
+		int e;
+		int c;
+		public Node(int e, int c) {
+			this.e = e;
+			this.c = c;
+		}
+		@Override
+		public int compareTo(Node o) {
+			return c-o.c;
 		}
 	}
 }
